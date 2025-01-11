e6d4e875597e56af8a69b0f53153024c57e9ae26
quanqvinh <nguyenquanqvinh@gmail.com>
2025-01-11 19:24:57 +0700

    feat: sharing prisma by using git submodule

--- prisma/migrations/20241209155558_update_column_table_series/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `slug` to the `Series` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE "Series" ADD COLUMN     "slug" VARCHAR(31) NOT NULL;
